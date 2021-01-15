db8fb1044f3f6078eb33d0c50babcc3d688cb3c7
Mohamed Ahmed <mohamedahmed315@hotmail.com>
2021-01-15 17:23:04 +0200

    Add files via upload

---
+++ reverse number.js
@@ -0,0 +1,12 @@
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+  readline.question(`Enter a string to reverse it: `, (number) => {
+    var reversedNumber = number.toString().split("").reverse().join("");
+    // var reversedNumber = parseInt(number.toString().split("").reverse().join(""));
+    console.log(reversedNumber);
+    readline.close()
+  })
